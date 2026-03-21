f40cbaab72503d4b615f593112050db7457c76ca
Sahil Mehta <sm2721857@gmail.com>
2026-03-21 20:25:32 +0530

    hide email credentials

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +84,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'EMAIL_HOST_USER', 'sm2721857@gmail.com'
-EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD', 'gcna exle aehp pdjv'
-DEFAULT_FROM_EMAIL = 'HostelHub <sm2721857@gmail.com>'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'HostelHub <hostel@gmail.com>')
 EMAIL_TIMEOUT = 5
